a4733841176f69241fb403c0456c038ebbb54537
Mahima <mahijain8376@gmail.com>
2024-01-03 23:30:19 +0530

    add use window resize hook and fix header

--- app/src/Components/Header/index.js
+++ app/src/Components/Header/index.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Avatar from '@mui/material/Avatar';
 import "./style.css";
 import dp from "../../assets/profile.jpeg";
+import MenuIcon from '@mui/icons-material/Menu';
+import useWindowSize from "../CustomHooks/useWindowSize";
 
 const Header = () => {
+    const isMobile = useWindowSize();
     return <div className="header">
+        {isMobile && <MenuIcon className="menuIcon" />}
         <Avatar src={dp} alt="profile-pic" />
         <h1>The Gram</h1>
     </div>
