d3148051215f572e1a3c5a3c25c7e5e668f151c9
Mahima <mahijain8376@gmail.com>
2024-01-07 18:23:55 +0530

    Add search component UI

--- app/src/Components/Search/index.js
+++ app/src/Components/Search/index.js
@@ -1,8 +1,45 @@
 import React from "react"
+import SearchIcon from "@mui/icons-material/Search";
+import "./style.css";
+import { Avatar, List, ListItem, ListItemAvatar, ListItemText } from "@mui/material";
+import dp from "../../assets/profile.jpeg";
 
 const Search = () => {
+    let userList = [{
+        dp: dp,
+        name: "Mahima Jain"
+    }, {
+        dp: dp,
+        name: "Mahima Jain"
+    }, {
+        dp: dp,
+        name: "Mahima Jain"
+    }, {
+        dp: dp,
+        name: "Mahima Jain"
+    }]
     return (
-        <div>Search Works</div>
+        <>
+            <div className="searchBar">
+                <input type="text" className="searchInput" placeholder="search" />
+                <SearchIcon />
+            </div>
+            <div className="userList">
+                <List>
+                    {userList.map(user => {
+                        return (
+                            <ListItem>
+                                <ListItemAvatar>
+                                    <Avatar src={user.dp} />
+                                </ListItemAvatar>
+                                <ListItemText>{user.name}</ListItemText>
+                            </ListItem>
+                        )
+                    })}
+                </List>
+            </div>
+        </>
+
     )
 }
 
