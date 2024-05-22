50a9ea167a2eea2f07ad30c541e711f12fb69bd2
Mahima <mahijain8376@gmail.com>
2024-05-22 23:14:45 +0530

    exclude logged in user from searchlist

--- server/router/user/getUsers.js
+++ server/router/user/getUsers.js
@@ -3,10 +3,17 @@ import User from "../../db/models/user.js";
 const getUsers = async (req, res) => {
     try {
         const q = req.query.q || '';
+        const loggedInUserId = req.user._id;
+
+        console.log("Search value--" + q);
+        // Exclude loggedin user
         const userList = await User.find({
             $or: [
                 { name: { $regex: new RegExp(q) } },
                 { userId: { $regex: new RegExp(q) } }
+            ],
+            $and: [
+                { _id: { $ne: loggedInUserId } }
             ]
         }).skip(0).limit(10);
         res.status(200).send({ users: userList })
