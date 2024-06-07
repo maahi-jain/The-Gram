937f1980718dd371638732a4a522686b8d9fc6c2
Mahima <mahijain8376@gmail.com>
2024-06-07 23:17:04 +0530

    update refrenced imports

--- app/src/index.js
+++ app/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App";
-import { AuthProvider } from "./customHooks/useAuthentication";
+import { AuthProvider } from "src/customHooks/useAuthentication";
 import store from "./store";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
