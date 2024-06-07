ed7b8f2634473cf88d2371c6bf4e5dd1af24900c
Mahima <mahijain8376@gmail.com>
2024-06-07 22:48:14 +0530

    update index.js

--- app/src/index.js
+++ app/src/index.js
@@ -1,10 +1,9 @@
 import React from "react";
-import App from "./App";
-import axios from './components/Service/axios-interceptor';
 import ReactDOM from "react-dom/client";
-import { AuthProvider } from "./customHooks/useAuthentication";
 import { Provider } from "react-redux";
-import store from "./store"
+import App from "./App";
+import { AuthProvider } from "./customHooks/useAuthentication.js";
+import store from "./store";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 const ConnectedApp = () => {
